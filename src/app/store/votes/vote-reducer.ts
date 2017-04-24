736ed3e23dccb8880e35e209decebbe279600b5e
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-04-24 10:53:16 -0500

    step 3: use action creators

--- src/app/store/votes/vote-reducer.ts
+++ src/app/store/votes/vote-reducer.ts
@@ -1,11 +1,10 @@
-import {Action} from 'redux';
-import {VoteActions} from './vote-actions';
+import { Action, VoteActions } from './vote-actions';
 import {INITIAL_VOTES_STATE, VotesState} from 'app/store/votes/vote-state';
 
 /**
  * Use the custom actions to update the counter state!
  */
-export function voteReducer(state: VotesState = INITIAL_VOTES_STATE, action:Action) {
+export function voteReducer(state: VotesState = INITIAL_VOTES_STATE, action: Action) {
   switch (action.type) {
     case VoteActions.NO  :  return {...state, counter: state.counter - 1};
     case VoteActions.YES :  return {...state, counter: state.counter + 1};
