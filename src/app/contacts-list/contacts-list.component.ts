09d23d8d2fc87244112eb63968b407c2c5adc8a3
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-05-03 22:53:33 +0200

    step 4: use takeUntil to mitigate slow initial response

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { debounceTime, distinctUntilChanged, merge, switchMap } from 'rxjs/operators';
+import { debounceTime, delay, distinctUntilChanged, merge, switchMap, takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs/Subject';
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
@@ -22,7 +22,10 @@ export class ContactsListComponent implements OnInit {
       debounceTime(400),
       distinctUntilChanged(),
       switchMap(term => this.contactsService.search(term)),
-      merge(this.contactsService.getContacts()),
+      merge(this.contactsService.getContacts().pipe(
+        delay(5000),
+        takeUntil(this.terms$)
+      ))
     );
   }
 
