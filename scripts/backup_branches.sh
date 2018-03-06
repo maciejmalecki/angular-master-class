a72b3b196df65d5b7dd7e9ea6fc0815752af7db5
Pascal Precht <pascal.precht@gmail.com>
2018-03-06 10:51:38 +0100

    step 0: initial commit

---
+++ scripts/backup_branches.sh
@@ -0,0 +1,8 @@
+git branch -f observables_backup observables
+git branch -f architecture_backup architecture
+git branch -f forms_backup forms
+git branch -f routing_backup routing
+git branch -f redux_backup redux
+git branch -f ngrx_backup ngrx
+git branch -f testing_backup testing
+git branch -f reusable-libraries_backup reusable-libraries
