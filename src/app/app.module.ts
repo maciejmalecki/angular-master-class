327f43fd9ad050ea96bc257f7ab6bbbed5190576
Pascal Precht <pascal.precht@gmail.com>
2018-04-03 14:42:49 +0200

    step 3: use tabs module in app sandbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsModule } from './modules/tabs/tabs.module';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    TabsModule
   ],
   bootstrap: [AppComponent]
 })
