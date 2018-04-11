mkk255/Angular2Excercises

step 6: async operations using effects

src/app/contacts-editor/contacts-editor.component.ts
@@ -4,11 +4,11 @@ import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 
 import { Contact } from '../models/contact';
-import { ContactsService } from '../contacts.service';
 
 import { ApplicationState } from '../state-management';
 import { UpdateContactAction } from '../state-management/contacts/contacts.actions';
 import { ContactsQuery } from '../state-management/contacts/contacts.reducer';
+import { SaveContactAction } from 'app/state-management/contacts/contacts.actions';
 
 @Component({
   selector: 'trm-contacts-editor',
@@ -19,9 +19,8 @@ export class ContactsEditorComponent implements OnInit {
 
   contact$: Observable<Contact>;
 
-  constructor(private contactsService: ContactsService,
-    private store: Store<ApplicationState>,
-    private router: Router) { }
+  constructor(private router: Router,
+    private store: Store<ApplicationState>) { }
 
   ngOnInit() {
     this.contact$ = this.store.select(ContactsQuery.getSelectedContact);
@@ -32,12 +31,7 @@ export class ContactsEditorComponent implements OnInit {
   }
 
   save(contact: Contact) {
-    this.contactsService
-      .updateContact(contact)
-      .subscribe(() => {
-        this.store.dispatch(new UpdateContactAction(contact));
-        this.goToDetails(contact);
-      });
+    this.store.dispatch(new SaveContactAction(contact));
   }
 
   private goToDetails(contact: Contact) {

src/app/state-management/contacts/contacts.actions.ts
@@ -14,10 +14,16 @@ import { Contact } from '../../models/contact';
  * required to mark the type property as `readonly` on the action class.
  */
 export namespace ContactsActionTypes {
+  export const LOAD_CONTACTS = '[Contacts] Load Contacts';
   export const LOAD_CONTACTS_SUCCESS = '[Contacts] Load Contacts Success';
   export const ADD_CONTACT = '[Contacts] Add Contact';
   export const UPDATE_CONTACT = '[Contacts] Update Contact';
+  export const SAVE_CONTACT = '[Contacts] Save Contact';
   export const SELECT_CONTACT = '[Contacts] Select Contact';
+}
+
+export class LoadContactsAction implements Action {
+  readonly type = ContactsActionTypes.LOAD_CONTACTS;
 }
 
 export class LoadContactsSuccessAction implements Action {
@@ -44,5 +50,11 @@ export class UpdateContactAction implements Action {
   constructor(public payload: Contact) { }
 }
 
-export type ContactsActions = LoadContactsSuccessAction | AddContactAction
-  | SelectContactAction | UpdateContactAction;
+export class SaveContactAction implements Action {
+  readonly type = ContactsActionTypes.SAVE_CONTACT;
+
+  constructor(public payload: Contact) { }
+}
+
+export type ContactsActions = LoadContactsAction | LoadContactsSuccessAction |
+  AddContactAction | SelectContactAction | UpdateContactAction | SaveContactAction;

maciejmalecki/angular-master-class

src/app/contacts-editor/contacts-editor.component.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 
 import { Contact } from '../models/contact';
-import { ContactsService } from '../contacts.service';
 
 import { ApplicationState } from '../state';
 import { UpdateContactAction } from '../state/contacts/contacts.actions';
@@ -19,9 +18,8 @@ export class ContactsEditorComponent implements OnInit {
 
   contact$: Observable<Contact>;
 
-  constructor(private contactsService: ContactsService,
-    private store: Store<ApplicationState>,
-    private router: Router) { }
+  constructor(private router: Router,
+    private store: Store<ApplicationState>) { }
 
   ngOnInit() {
     this.contact$ = this.store.select(ContactsQuery.getSelectedContact);
@@ -32,12 +30,7 @@ export class ContactsEditorComponent implements OnInit {
   }
 
   save(contact: Contact) {
-    this.contactsService
-      .updateContact(contact)
-      .subscribe(() => {
-        this.store.dispatch(new UpdateContactAction(contact));
-        this.goToDetails(contact);
-      });
+    this.store.dispatch(new UpdateContactAction(contact));
   }
 
   private goToDetails(contact: Contact) {

src/app/state/contacts/contacts.actions.ts
@@ -13,15 +13,31 @@ import { Contact } from '../../models/contact';
  * sure to also mark the `type` property as `readonly` on the action class.
  */
 export enum ContactsActionTypes {
+  LOAD_CONTACTS = '[Contacts] Load Contacts',
   LOAD_CONTACTS_SUCCESS = '[Contacts] Load Contacts Success',
   UPDATE_CONTACT = '[Contacts] Update Contact',
+  UPDATE_CONTACT_SUCCESS = '[Contacts] Update Contact Success',
   SELECT_CONTACT = '[Contacts] Select Contact',
   ADD_CONTACT = '[Contacts] Add Contact'
+}
+
+export class LoadContactsAction implements Action {
+  readonly type = ContactsActionTypes.LOAD_CONTACTS;
 }
 
 export class LoadContactsSuccessAction implements Action {
   readonly type = ContactsActionTypes.LOAD_CONTACTS_SUCCESS;
   constructor(public payload: Array<Contact>) { }
+}
+
+export class UpdateContactAction implements Action {
+  readonly type = ContactsActionTypes.UPDATE_CONTACT;
+  constructor(public payload: Contact) { }
+}
+
+export class UpdateContactSuccessAction implements Action {
+  readonly type = ContactsActionTypes.UPDATE_CONTACT_SUCCESS;
+  constructor(public payload: Contact) { }
 }
 
 export class SelectContactAction implements Action {
@@ -34,11 +50,6 @@ export class AddContactAction implements Action {
   constructor(public payload: Contact) { }
 }
 
-export class UpdateContactAction implements Action {
-  readonly type = ContactsActionTypes.UPDATE_CONTACT;
-  constructor(public payload: Contact) { }
-}
-
 export type ContactsActions =
-  LoadContactsSuccessAction | UpdateContactAction | AddContactAction |
-  SelectContactAction;
+  LoadContactsSuccessAction | LoadContactsSuccessAction | UpdateContactAction |
+  UpdateContactSuccessAction | AddContactAction | SelectContactAction;