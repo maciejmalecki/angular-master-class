mkk255/Angular2Excercises

step 1: basic ngrx setup

src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 import { ContactsAppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
@@ -16,6 +17,8 @@ import { ContactsService } from './contacts.service';
 
 import { APP_ROUTES } from './app.routes';
 import { API_ENDPOINT } from './app.tokens';
+
+import { ROOT_REDUCER } from './state-management';
 
 @NgModule({
   declarations: [
@@ -30,6 +33,7 @@ import { API_ENDPOINT } from './app.tokens';
     MaterialModule,
     FlexLayoutModule,
     RouterModule.forRoot(APP_ROUTES),
+    StoreModule.provideStore(ROOT_REDUCER),
     HttpModule,
     FormsModule
   ],

maciejmalecki/angular-master-class

src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 import { ContactsMaterialModule } from './contacts-material.module';
 
@@ -17,6 +18,8 @@ import { ContactsService } from './contacts.service';
 
 import { APP_ROUTES } from './app.routes';
 import { API_ENDPOINT } from './app.tokens';
+
+import { ROOT_REDUCER } from './state';
 
 @NgModule({
   declarations: [
@@ -32,6 +35,7 @@ import { API_ENDPOINT } from './app.tokens';
     FlexLayoutModule,
     RouterModule.forRoot(APP_ROUTES),
     HttpClientModule,
+    StoreModule.forRoot(ROOT_REDUCER),
     FormsModule
   ],
   providers: [