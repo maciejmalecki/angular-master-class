mkk255/Angular2Excercises

step 1: introduce sidenav and about page using children routes

src/app/about/about.component.ts
@@ -1,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trm-about',
+  template: `
+    <div class="trm-about">
+      <md-card fxLayout="column" fxFlex fxLayoutAlign="center center">
+        <h2 md-card-title>Angular Master Class</h2>
+        <md-card-content>
+          <img src="/assets/images/team.jpg" alt="Team thoughtram">
+          <p style="text-align: center;">Brought to you by thoughtram</p>
+        </md-card-content>
+        <md-card-actions>
+          <a md-button title="Go back to Dashboard" routerLink="/">Go Back</a>
+        </md-card-actions>
+      </md-card>
+    </div>
+  `,
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {}

src/app/app.routes.ts
@@ -1,12 +1,20 @@
 import { Routes } from '@angular/router';
-import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { ContactsDetailViewComponent } from './contacts-detail-view/contacts-detail-view.component';
 import { ContactsEditorComponent } from './contacts-editor/contacts-editor.component';
+import { ContactsDashboardComponent } from './contacts-dashboard/contacts-dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: ContactsListComponent },
-  { path: 'contact/:id', component: ContactsDetailViewComponent },
-  { path: 'contact/:id/edit', component: ContactsEditorComponent },
+  {
+    path: '',
+    component: ContactsDashboardComponent,
+    children: [
+      { path: '', redirectTo: 'contact/0', pathMatch:'full' },
+      { path: 'contact/:id', component: ContactsDetailViewComponent },
+      { path: 'contact/:id/edit', component: ContactsEditorComponent }
+    ]
+  },
+  { path: 'about', component: AboutComponent },
   // Wildcard route serves as fallback route and has to be
   // the last defined route in this list.
   { path: '**', redirectTo: '/' }

maciejmalecki/angular-master-class

src/app/about/about.component.ts
@@ -1,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trm-about',
+  template: `
+    <div class="trm-about">
+      <mat-card fxLayout="column" fxFlex fxLayoutAlign="center center">
+        <h2 mat-card-title>Angular Master Class</h2>
+        <mat-card-content>
+          <img src="/assets/images/team.jpg" alt="Team thoughtram">
+          <p style="text-align: center;">Brought to you by thoughtram</p>
+        </mat-card-content>
+        <mat-card-actions>
+          <a mat-button title="Go back to Dashboard" routerLink="/">Go Back</a>
+        </mat-card-actions>
+      </mat-card>
+    </div>
+  `,
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {}

src/app/app.routes.ts
@@ -1,12 +1,20 @@
 import { Routes } from '@angular/router';
-import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { ContactsDetailComponent } from './contacts-detail/contacts-detail.component';
 import { ContactsEditorComponent } from './contacts-editor/contacts-editor.component';
+import { ContactsDashboardComponent } from './contacts-dashboard/contacts-dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: ContactsListComponent },
-  { path: 'contact/:id', component: ContactsDetailComponent },
-  { path: 'contact/:id/edit', component: ContactsEditorComponent },
+  {
+    path: '',
+    component: ContactsDashboardComponent,
+    children: [
+      { path: '', redirectTo: 'contact/0', pathMatch:'full' },
+      { path: 'contact/:id', component: ContactsDetailComponent },
+      { path: 'contact/:id/edit', component: ContactsEditorComponent }
+    ]
+  },
+  { path: 'about', component: AboutComponent },
   // Wildcard route serves as fallback route and has to be
   // the last defined route in this list.
   { path: '**', redirectTo: '/' }