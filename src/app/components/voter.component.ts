torstenwerner/angular-master-class-starter

step 0b: initial setup for standalone Redux app

src/app/components/voter.component.ts
@@ -1,0 +1,39 @@
+import { Component } from '@angular/core';
+import { AppStore } from '../store/app-store';
+
+@Component({
+  selector: 'trm-voter',
+  template: `
+    <div fxLayout="column" fxLayoutAlign="space-between center" fxFlex style="padding-left:10px;">
+      <button md-fab class="yes" mdTooltip="Vote Yes!">
+        <md-icon class="md-24">thumb_up</md-icon>
+      </button>
+
+      <ng-content></ng-content>
+
+      <button md-fab class="no" mdTooltip="Vote No!">
+        <md-icon class="md-24">thumb_down</md-icon>
+      </button>
+    </div>
+  `,
+  styles : [
+    `button.yes { background-color: darkgreen; }`,
+    `button.no { background-color: red; }`
+  ]
+})
+export class VoterComponent {
+
+  /**
+   * Inject the appstore
+   */
+  constructor(private store: AppStore) { }
+
+  private increment() {
+    // @Todo - dispatch action to the store
+  }
+
+  private decrement() {
+    // @Todo - dispatch action to the store
+  }
+}
+

maciejmalecki/angular-master-class

src/app/components/voter.component.ts
@@ -1,0 +1,39 @@
+import { Component } from '@angular/core';
+import { AppStore } from '../store/app-store';
+
+@Component({
+  selector: 'trm-voter',
+  template: `
+    <div fxLayout="column" fxLayoutAlign="space-between center" fxFlex style="padding-left:10px;">
+      <button mat-fab class="yes" matTooltip="Vote Yes!">
+        <mat-icon class="md-24">thumb_up</mat-icon>
+      </button>
+
+      <ng-content></ng-content>
+
+      <button mat-fab class="no" matTooltip="Vote No!">
+        <mat-icon class="md-24">thumb_down</mat-icon>
+      </button>
+    </div>
+  `,
+  styles : [
+    `button.yes { background-color: darkgreen; }`,
+    `button.no { background-color: red; }`
+  ]
+})
+export class VoterComponent {
+
+  /**
+   * Inject the appstore
+   */
+  constructor(private store: AppStore) { }
+
+  private increment() {
+    // @Todo - dispatch action to the store
+  }
+
+  private decrement() {
+    // @Todo - dispatch action to the store
+  }
+}
+